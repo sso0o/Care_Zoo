6e1a0a429c5101c8bad9693fd30cbc8e9bdefa9b
bitcamp <bitcamp@DESKTOP-SKSCPNQ>
2019-08-21 16:50:22 +0900

    + dao 츄가 쏘

--- CareZoo/src/main/java/com/what/carezoo/hotel/dao/HotelCommentDao.java
+++ CareZoo/src/main/java/com/what/carezoo/hotel/dao/HotelCommentDao.java
@@ -1,5 +1,16 @@
 package com.what.carezoo.hotel.dao;
 
+import java.util.List;
+
+import com.what.carezoo.model.HotelComment;
+
 public interface HotelCommentDao {
+	
+	public int insert(HotelComment hc);
+	public int update(HotelComment hc);
+	public int delete(HotelComment hc);
+	public HotelComment selectOne(int num);
+	public List<HotelComment> selectAll();
+	
 
 }
--- CareZoo/src/main/java/com/what/carezoo/hotel/dao/PetHotelComment.java
+++
@@ -1,5 +0,0 @@
-package com.what.carezoo.hotel.dao;
-
-public interface PetHotelComment {
-
-}
