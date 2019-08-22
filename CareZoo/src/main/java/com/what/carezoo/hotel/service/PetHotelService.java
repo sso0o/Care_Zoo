4a91e473b476fcba67111e34207854d7e1316575
bitcamp <bitcamp@DESKTOP-SKSCPNQ>
2019-08-22 18:54:09 +0900

    o0o

--- CareZoo/src/main/java/com/what/carezoo/hotel/service/PetHotelService.java
+++ CareZoo/src/main/java/com/what/carezoo/hotel/service/PetHotelService.java
@@ -1,5 +1,7 @@
 package com.what.carezoo.hotel.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,7 +35,12 @@ public class PetHotelService {
 		return false;
 	}
 	
+	public PetHotel getPetHotelbyNum(int num) {
+		return petHotelDao.selectOne(num);
+	}
 	
-	
+	public List<PetHotel> getAllPetHotel() {
+		return petHotelDao.selectAll();
+	}
 
 }
