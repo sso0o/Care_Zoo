ec397f1064048bebd40b6852cf000d69dc7a7e8b
bitcamp <bitcamp@DESKTOP-SKSCPNQ>
2019-08-22 19:53:42 +0900

    + petcontroller sso

--- CareZoo/src/main/java/com/what/carezoo/pet/dao/PetDao.java
+++ CareZoo/src/main/java/com/what/carezoo/pet/dao/PetDao.java
@@ -1,5 +1,15 @@
 package com.what.carezoo.pet.dao;
 
+import java.util.List;
+
+import com.what.carezoo.model.Pet;
+
 public interface PetDao {
+	
+	public int insertPet(Pet pet);
+	public int updatePet(Pet pet);
+	public int deletePet(Pet pet);
+	public Pet selectPet(int num);
+	public List<Pet> selectAllPet();
 
 }
