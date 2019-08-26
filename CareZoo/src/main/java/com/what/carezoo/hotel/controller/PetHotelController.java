d745427e3155596921d39287d0b56fa8c669ac2c
bitcamp <bitcamp@DESKTOP-SKSCPNQ>
2019-08-26 11:00:55 +0900

    필요없는 클래스 삭제

--- CareZoo/src/main/java/com/what/carezoo/hotel/controller/PetHotelController.java
+++ CareZoo/src/main/java/com/what/carezoo/hotel/controller/PetHotelController.java
@@ -7,14 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/petHotel")
 public class PetHotelController {//보호자 비동반 애견호텔 컨트롤러
 	
+	//펫호텔 목록보기
 	@RequestMapping("/petHotelList")
 	public String showPetHotelList() {
 		return "hotel/petHotelList";
 	}
 	
-	
+	//펫호텔 예약폼
 	@RequestMapping("/petHotelResForm")
 	public String showPetHotelResForm() {
 		return "hotel/petHotelResForm";
+	}
+	
+	//펫호텔 상세보기
+	@RequestMapping("/petHotelView")
+	public String showPetHotelView() {
+		return "hotel/petHotelView";
 	}
 }
