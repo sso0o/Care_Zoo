43dc750ed9a4a87918e95b0d8ef63deba9a2f156
bitcamp <bitcamp@DESKTOP-BIJGG7L>
2019-08-29 14:46:43 +0900

    hh

--- CareZoo/src/main/java/com/what/carezoo/dao/Pet_DetailDao.java
+++ CareZoo/src/main/java/com/what/carezoo/dao/Pet_DetailDao.java
@@ -1,7 +1,10 @@
 package com.what.carezoo.dao;
 
+import java.util.List;
+
 import com.what.carezoo.model.Pet_Detail;
 
 public interface Pet_DetailDao {
 	public int insertPet_DetailDao(Pet_Detail pet_detail);
+	public List<Pet_Detail> selectByP_Num(int p_num);
 }
