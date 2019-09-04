6ec6ba5e22bb6607117aff24bb27ad5c7c970c9d
bitcamp <bitcamp@DESKTOP-I815GUP>
2019-09-04 19:40:08 +0900

    +homesitter:heejin

--- CareZoo/src/main/java/com/what/carezoo/dao/HomeSitterListDao.java
+++ CareZoo/src/main/java/com/what/carezoo/dao/HomeSitterListDao.java
@@ -2,6 +2,8 @@ package com.what.carezoo.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.what.carezoo.model.HomeSitterList;
 
 public interface HomeSitterListDao {
@@ -12,5 +14,7 @@ public interface HomeSitterListDao {
 	public HomeSitterList selectOnebyHsl_num(int hsl_num);
 	public HomeSitterList searchHsl(HomeSitterList hsl);
 	public List<HomeSitterList> searchHslbyAddress(List<String> hsl_address);
-	public List<HomeSitterList> selectAllHsl(HomeSitterList hsl);
+//	public List<HomeSitterList> selectAllHsl(HomeSitterList hsl);
+	
+	public List<HomeSitterList> selectAllHsl(@Param("list")List<String> hsl_address, @Param("hsl")HomeSitterList hsl);
 }
