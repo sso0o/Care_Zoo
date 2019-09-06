7dbb970478785505d3fb136fa8e4aeefdc22626c
github_username <gomlng@naver.com>
2019-09-06 17:04:26 +0900

    yj adminDao,mapper

---
+++ CareZoo/src/main/java/com/what/carezoo/dao/AdminDao.java
@@ -0,0 +1,15 @@
+package com.what.carezoo.dao;
+
+import java.util.List;
+
+import com.what.carezoo.model.Admin;
+
+
+public interface AdminDao {
+   public int insert(Admin a);
+   public int update(Admin a);
+   public int delete(Admin a);
+   public Admin selectOneByEmail(String adm_email);
+   public List<Admin> selectAllAdmin();
+   
+}
