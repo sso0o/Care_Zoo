601022c8f69e62336272edd70cf44ac99d441e98
bitcamp <bitcamp@DESKTOP-I815GUP>
2019-09-17 09:49:19 +0900

    +HOMESITTER:희진

--- CareZoo/src/main/java/com/what/carezoo/dao/HomeSitterReservationDao.java
+++ CareZoo/src/main/java/com/what/carezoo/dao/HomeSitterReservationDao.java
@@ -1,5 +1,9 @@
 package com.what.carezoo.dao;
 
 public interface HomeSitterReservationDao {
-
+	public int insert    ();
+	public int update    ();
+	public int delete    ();
+	public int select    ();
+	public int selectAll ();
 }
