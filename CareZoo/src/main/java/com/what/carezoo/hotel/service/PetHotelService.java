7ba6fb9c8fa67e271bd79f889acf4e79ab5a87c8
bitcamp <bitcamp@DESKTOP-SKSCPNQ>
2019-09-17 14:26:21 +0900

    ssss

--- CareZoo/src/main/java/com/what/carezoo/hotel/service/PetHotelService.java
+++ CareZoo/src/main/java/com/what/carezoo/hotel/service/PetHotelService.java
@@ -205,7 +205,7 @@ public class PetHotelService {
 	}
 
 	public PetHotelReservation getOnePetHotelRes(int phr_num) {
-		return petHotelResDao.selectOneByNum(phr_num);
+		return petHotelResDao.selectByPhrnum(phr_num);
 
 	}
 
