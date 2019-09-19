6e3cd36d44cd1bfcbda0565b2a0a784c3b7e486d
bitcamp <bitcamp@DESKTOP-I815GUP>
2019-09-19 10:01:52 +0900

    +homesitter : 희진

--- CareZoo/src/main/java/com/what/carezoo/member/controller/MemberController.java
+++ CareZoo/src/main/java/com/what/carezoo/member/controller/MemberController.java
@@ -7,21 +7,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.what.carezoo.hotel.service.PetHotelReservationService;
 import com.what.carezoo.hotel.service.PetHotelService;
 import com.what.carezoo.member.service.MemberService;
-import com.what.carezoo.model.Customer;
+
 import com.what.carezoo.model.HomeSitter;
 import com.what.carezoo.model.HomeSitterReservation;
 import com.what.carezoo.model.Pet;
